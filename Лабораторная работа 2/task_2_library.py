237879090f377899051bbfb77c6e95c131043595
Danzhuk <101832100+chsvc@users.noreply.github.com>
2024-02-06 09:16:22 +0300

    Update task_2_library.py

--- Лабораторная работа 2/task_2_library.py
+++ Лабораторная работа 2/task_2_library.py
@@ -36,7 +36,7 @@ class Library:
             self.books = books
 
     def get_next_book_id(self):
-        if len(self.books) == 0:
+        if not self.books:
             return 1
         else:
             last_book = self.books[-1]
